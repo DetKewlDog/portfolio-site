e0738b77cda0e91803aa1ede06580d63f46b2f41
Bar Goldenstein <bargolden12@gmail.com>
2023-07-16 12:00:15 +0300

    modified animatedlink

--- src/components/AnimatedLink.jsx
+++ src/components/AnimatedLink.jsx
@@ -3,21 +3,22 @@ import { useNavigate} from 'react-router-dom';
 // THANK YOU DOT FOR THE TRANSITIONS
 export default function AnimatedLink({ to, children }) {
 	const navigate = useNavigate();
+	const handleClick = async(e) => {
+		e.preventDefault();
+		if (!document.startViewTransition || window.location.pathname === to) return navigate(to);
+		try {
+			await document.startViewTransition(() => {
+				navigate(to);
+				document.documentElement.scrollTo(0, 0);
+			}).finished;
+		}
+		catch {
+			navigate(to);
+		}
+	}
 	return (
 		<li>
-			<a href={to}
-				onClick={async (e) => {
-					e.preventDefault();
-					if (!document.startViewTransition || window.location.pathname === to) return navigate(to);
-					try {
-						await document.startViewTransition(() => {
-							navigate(to);
-							document.documentElement.scrollTo(0, 0);
-						}).finished;
-					}
-					catch { navigate(to) }
-				}}
-			>
+			<a href={to} onClick={handleClick}>
 				{children}
 			</a>
 		</li>
