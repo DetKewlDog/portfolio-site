a19a600bb9884ff3b4c1299b3b4d3dd15e768a2a
Bar Goldenstein <bargolden12@gmail.com>
2023-07-20 17:37:16 +0300

    added <nav>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,25 @@ import SocialLink from "./SocialLink";
 export default function Navbar() {
 
     return (
-        <div className="navbar">
-            <ul>
-                <li><span className="unselectable"><b>DetKewlDog</b></span></li>
-                <AnimatedLink to="/">Home</AnimatedLink>
-                <AnimatedLink to="/projects">Projects</AnimatedLink>
+        <nav>
+            <div className="navbar">
+                <ul>
+                    <li><span className="unselectable"><b>DetKewlDog</b></span></li>
+                    <AnimatedLink to="/">Home</AnimatedLink>
+                    <AnimatedLink to="/projects">Projects</AnimatedLink>
 
-                <SocialLink icon="steam"     url="https://steamcommunity.com/id/DetKewlDog/" />
-                <SocialLink icon="reddit"    url="https://www.reddit.com/user/DetKewlDog"    />
-                <SocialLink icon="discord"   url="https://discord.com/invite/F7VQ2hwfyw"     />
-                <SocialLink icon="youtube"   url="https://youtube.com/@autumnfire_dev"       />
-                <SocialLink icon="github"    url="https://github.com/DetKewlDog"             />
-                <li>
-                    <a className="fas fa-bars" aria-hidden="false" name="Socials">
-                        <input type="checkbox" />
-                    </a>
-                </li>
-            </ul>
-        </div>
+                    <SocialLink icon="steam"     url="https://steamcommunity.com/id/DetKewlDog/" />
+                    <SocialLink icon="reddit"    url="https://www.reddit.com/user/DetKewlDog"    />
+                    <SocialLink icon="discord"   url="https://discord.com/invite/F7VQ2hwfyw"     />
+                    <SocialLink icon="youtube"   url="https://youtube.com/@autumnfire_dev"       />
+                    <SocialLink icon="github"    url="https://github.com/DetKewlDog"             />
+                    <li>
+                        <a className="fas fa-bars" aria-hidden="false" name="Socials">
+                            <input type="checkbox" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
