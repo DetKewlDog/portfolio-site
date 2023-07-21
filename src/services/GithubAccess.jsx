2f38d2b37810245ea2d58b4acdeb2a40b7fbaf03
Bar Goldenstein <bargolden12@gmail.com>
2023-07-21 10:24:02 +0300

    fixed outdated id

--- src/services/GithubAccess.jsx
+++ src/services/GithubAccess.jsx
@@ -8,7 +8,7 @@ export default class GitHubAccess {
             const data = await response.text();
             // using a timestamp in order to make the stats graph unique every time, so it wont be cached and would animate every time
             const timestamp = new Date().getTime();
-            return decodeURI(data.replace('" alt="Top Lang"', `?${timestamp}" alt="Top Lang"`));
+            return decodeURI(data.replace('" alt="Things I\'d Like to Learn"', `?${timestamp}" alt="Top Lang"`));
         } catch (error) {
             console.error(error)
             return 'Error fetching about me.';
