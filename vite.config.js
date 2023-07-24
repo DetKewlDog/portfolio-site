9def1b9d6b062169d225215fa9a74c1605acd3c2
Bar Goldenstein <bargolden12@gmail.com>
2023-07-24 11:52:36 +0300

    got rid of something

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cdn } from 'vite-plugin-cdn2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    cdn({ modules: ['react'] })
+    react()
   ],
 })
